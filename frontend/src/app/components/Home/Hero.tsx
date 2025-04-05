9066b09e1adff79c449aa93f3c2080b0ffbe99e2
Prathmesh kulkarni <prathmeshkulkarni312@gmail.com>
2025-04-05 15:59:50 +0530

    new

--- frontend/src/app/components/Home/Hero.tsx
+++ frontend/src/app/components/Home/Hero.tsx
@@ -2,7 +2,7 @@
 
 export default function Hero() {
     return ( <>
-        <section className="p-8 text-3xl font-bold">This is the Hero section</section> 
+        <section className="p-8 text-3xl font-bold">This is the Hero section new</section> 
         </>
     );
   }
--- frontend/src/app/signup/page.tsx
+++ frontend/src/app/signup/page.tsx
@@ -3,7 +3,7 @@
 export default function SignupPage() {
   return (
     <div className="p-6">
-      <h1 className="text-2xl font-bold text-pink-700">🧾 Signup Page</h1>
+      <h1 className="text-2xl font-bold text-pink-700">🧾 new Signup Page</h1>
       
     </div>
   );
